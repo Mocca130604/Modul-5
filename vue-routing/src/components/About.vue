e97cb8517acf0e9ac32d4293f869053ee8ac6445
Mocca13 <medioid13@gmail.com>
2021-03-10 15:29:44 +0700

    modul 5 vue routing

---
+++ vue-routing/src/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>Tentang</h1>
+        This Is About this Page
+    </div>
+</template>
+
+<script>
+export default {
+    name: "About"
+}
+</script>
---
+++ vue-routing/src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>Beranda</h1>
+        This Is Home Page
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
